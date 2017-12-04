e0f40d34a759b2dd9d00491d71b6759e331d20d1
Enner Pérez <ennerperez@gmail.com>
2017-12-03 23:23:01 -0400

    Mejoras de estructura y estilo

--- SoftSupply-Help/Controllers/DefaultController.cs
+++ SoftSupply-Help/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoftSupply.Help.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,28 @@ namespace SoftSupply.Help.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Search(string area, string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+
+                var contents = new System.IO.DirectoryInfo(Server.MapPath($"Areas\\{area}"));
+                var files = contents.GetFiles("*.cshtml", System.IO.SearchOption.AllDirectories);
+
+                var result = (from item in files
+                              let body = System.IO.File.ReadAllText(item.FullName)
+                              where body.Contains(searchString)
+                              select item).FirstOrDefault();
+
+                if (result != null)
+                    return Redirect($"{area}?id={result.Directory.Name}&key={System.IO.Path.GetFileNameWithoutExtension(result.Name)}");
+
+            }
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
     }
 }
